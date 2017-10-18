9fcd0eee2ddc53e1e8101205597a354a4f8ff9d1
Denys Vitali <denys.vitali@coelis.ch>
2017-10-18 21:12:51 +0200

    Pixel Features

--- config.sh
+++ config.sh
@@ -42,7 +42,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "   Pixel 2 IntelligenceSense   "
   ui_print "*******************************"
 }
 
